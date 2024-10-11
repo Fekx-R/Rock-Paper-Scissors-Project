2792c5e2c3e00d10493fbe9405229789c3871438
Fekx-R <fikriramadhanofficial18@gmail.com>
2024-10-11 14:43:47 +0800

    step 4 finish

---
+++ script.js
@@ -0,0 +1,19 @@
+humanScore = 0;
+computerScore = 0;
+
+const getComputerChoice = (max) => {
+    return Math.floor(Math.random() * max);
+}
+console.log(getComputerChoice(2));
+
+
+const getHumanChoice = () => {
+    return prompt(`Choose: Rock, Paper, Scissor`, "Paper");
+}
+console.log(getHumanChoice());
+
+const playRound = (humanChoice, computerChoice) => {
+    humanChoice = //i
+    console.log();
+    
+}
